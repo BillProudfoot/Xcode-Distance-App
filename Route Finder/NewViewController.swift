d862a428b4c892d46e2906e121950efa38bb6fa3
Bill Proudfoot <billproudfoot@Bills-MacBook-Pro.local>
2017-09-12 17:34:33 +0100

    distance calculator app

---
+++ Route Finder/NewViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewViewController.swift
+//  Route Finder
+//
+//  Created by Bill Proudfoot on 08/09/2017.
+//  Copyright © 2017 Bill Proudfoot. All rights reserved.
+//
+
+import Foundation
